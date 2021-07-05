25f7103075a6cd6fc176096bdd8d3c6dbda99b50
Khushbu Solanki <khushbu.solanki@ekzero.com>
2021-07-05 21:53:29 +0530

    Remove unwanted code

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,57 +1,9 @@
 import { combineReducers, createStore, applyMiddleware, compose  } from 'redux';
 import { listReducer } from './Reducer';
-import rootSaga from './rootSaga';
-import createSagaMiddleware from 'redux-saga';
-import { createLogger } from 'redux-logger';
-import { createNetworkMiddleware } from 'react-native-offline'
 
 const rootReducer = combineReducers({ 
         gitHub : listReducer 
     });
-
-const mainRootReducer = (state, action) => {
-    return rootReducer(state, action);
-}
-
-// const configureStore = (rootReducer, rootsaga) =>{
-//     /* --------------Redux Configuration ---------------- */
-//     const middleware = []
-//     const enhancers = []
-
-//     /* --------------Network Middleware -------------------*/
-//     const networkMiddleware = createNetworkMiddleware({
-//         queueReleaseThrottle: 200,
-//     })
-//     middleware.push(networkMiddleware)
-
-//     /* --------------Saga Middleware----------------*/
-//     const sagaMiddleware = createSagaMiddleware()
-//     middleware.push(sagaMiddleware)
-
-//     /* --------------Logger Middleware---------------- */
-//     if (__DEV__){
-//         middleware.push(createLogger())
-//     }
-
-//     /* --------------Assemble Middleware--------------- */
-//     enhancers.push(applyMiddleware(...middleware))
-
-//     /* --------------- AutoRehydrate Enhancer -------------- */
-//     const initialState = {};
-//     const store = createStore(rootReducer, initialState, compose(...enhancers))
-//     sagaMiddleware.run(rootsaga)
-
-//     return store
-// }
-
-// const createReduxStore = () => {
-//     return configureStore(mainRootReducer,rootSaga)
-// }
-
-// const store = createReduxStore()
-
-// export default store
-
 
 const configureStore = () => {
     return createStore(rootReducer);
